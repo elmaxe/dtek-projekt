13f0b560c9400e90b418a1b98843f9a258ff45ba
Axel Elmarsson <axel.elmarsson@gmail.com>
2018-11-28 13:22:38 +0100

    Set game title to 'Dinosaur Run'

--- src/main.c
+++ src/main.c
@@ -55,7 +55,7 @@ int main(void) {
 	SPI2CONSET = 0x8000;
 
 	display_init();
-	display_string(0, "Title");
+	display_string(0, "Dinosaur Run");
 	display_string(1, "M.Wesslen");
 	display_string(2, "A.Elmarsson");
 	display_string(3, "Press any button");
