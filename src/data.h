a9beb246be7ebb411ea1fd9526d273b38baedccb
Markus Wesslén <markus.wesslen@gmail.com>
2018-12-04 15:31:05 +0100

    Added graphic size constants

--- src/data.h
+++ src/data.h
@@ -1,3 +1,12 @@
+#define CACTUS_HEIGHT 13
+#define CACTUS_WIDTH 6
+#define BIRD_HEIGHT 9
+#define BIRD_WIDTH 13
+#define DINO_HEIGHT 16
+#define DINO_WIDTH 16
+#define GROUND_HEIGHT 4
+#define GROUND_WIDTH 128
+
 /* Declare bitmap array containing font */
 extern const uint8_t const font[128*8];
 /* Declare text buffer for display output */
